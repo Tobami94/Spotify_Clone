5c5d20a9aa638acf8097b45a042722bba1660c53
unknown <malenaparaschuk@gmail.com>
2024-07-08 14:53:34 +0200

    08/7/24

---
+++ src/components/PageGeneral/Greeting.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+const currentTime = new Date()
+const currentHour = currentTime.getHours()
+
+let greeting = ""
+
+
+
+if (currentHour < 12) {
+    greeting = "Buenos días"
+} else if (currentHour > 12) {
+    greeting = "Buenas tardes"
+} else  {
+greeting = "Buenas Noches"
+}
+
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
